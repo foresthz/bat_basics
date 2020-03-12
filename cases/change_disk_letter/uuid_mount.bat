7fd647dd840f82dd983782782947a26f9eb693f6
host <host@host.com>
2020-03-12 22:18:10 +0800

    mod

--- cases/change_disk_letter/uuid_mount.bat
+++ cases/change_disk_letter/uuid_mount.bat
@@ -1,6 +1,9 @@
 :: d:根目录下生成了2个uuid文件,之后可以删除 
+
+set target_letter=m:
+
 ping -n 10 1.1.1.22
 set /p uuid_str=<d:\uuid_dsk.txt
-mountvol m: %uuid_str%
+mountvol %target_letter% %uuid_str%
 
 start d:
